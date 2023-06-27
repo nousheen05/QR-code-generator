19d339ba1afbed40da95d77d6b6b7cd016f16dd9
nousheen05 <21211a05k3@bvrit.ac.in>
2023-06-27 19:22:44 +0530

    terminal project ended

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-
-
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
 
 inquirer
   .prompt([
-    
+    {
+      message:"Enter URL link: ",
+      name: "URL"
+    }
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const ans = answers.URL;     
+    
+    //the url is added to file
+    fs.writeFile('url.txt', ans, function(err)
+    {
+      if(err) throw err;
+    });
+
+    //creating qr image png
+    var qr_svg = qr.image(ans, { type: 'png' }); // or 'svg' formats
+    qr_svg.pipe(fs.createWriteStream('qr.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
